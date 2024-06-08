732f342ca90361618e116bcc3573045d1cd15d98
Yehanrcg <155339196+Yehanrcg@users.noreply.github.com>
2024-06-08 18:51:16 +0530

    StackAsLinkedList.java

---
+++ Stack/StackAsLinkedList.java
@@ -0,0 +1,90 @@
+
+
+public class StackAsLinkedList {
+
+    StackNode root;
+
+    static class StackNode {
+        int data;
+        StackNode next;
+
+        StackNode(int data) { this.data = data; }
+    }
+
+    public boolean isEmpty()
+    {
+        if (root == null) {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void push(int data)
+    {
+        StackNode newNode = new StackNode(data);
+
+        if (root == null) {
+            root = newNode;
+        }
+        else {
+            StackNode temp = root;
+            root = newNode;
+            newNode.next = temp;
+        }
+        System.out.println(data + " pushed to stack");
+    }
+
+    public int pop()
+    {
+        int popped = Integer.MIN_VALUE;
+        if (root == null) {
+            System.out.println("Stack is Empty");
+        }
+        else {
+            popped = root.data;
+            root = root.next;
+        }
+        return popped;
+    }
+
+    public int peek()
+    {
+        if (root == null) {
+            System.out.println("Stack is empty");
+            return Integer.MIN_VALUE;
+        }
+        else {
+            return root.data;
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+
+        StackAsLinkedList stack = new StackAsLinkedList();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        System.out.println(stack.pop()
+                           + " popped from stack");
+
+        System.out.println("Top element is " + stack.peek());
+    
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+
+        System.out.println(stack.pop()
+                           + " popped from stack");
+
+        System.out.println("Top element is " + stack.peek());
+    }
+}
